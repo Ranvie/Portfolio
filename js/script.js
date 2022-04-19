aafcfc56e4a82f840f37a84312559d0a11ad406d
Ranvie <rangelvieira33@gmail.com>
2022-04-19 00:07:10 -0300

    70% da funcionalidade de Ler Mais implementada

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+window.onload = function(){
+    UpdateDiv.update('#head_content', 'pages/biography.html', 'biography', 'head_menu_selected'); 
+    UpdateDiv.update('#body_content_container', 'pages/post.html', 'post', 'body_menu_selected');
+}
+
+function set_up_read_more(){
+    var postElem = document.getElementsByClassName("post_content");
+    var postLink = document.getElementsByClassName("link_read_more");
+    
+    for(var a=0; a < postElem.length; a++){
+        postElem[a].setAttribute("id", "post#" + (a+1));
+        postLink[a].setAttribute("id", "post_link#" + (a+1));
+    }
+
+}
+
+function read_more(e){
+    alert(e.id);
+}
+
 var UpdateDiv = {
 
     //Update div without reloading entire page;
@@ -16,13 +36,25 @@ var UpdateDiv = {
     update: function(id, url, element, className){
 
         $(id).load(url, function(data, textStatus, xhr){
-            if(textStatus == "success"){
+            if(textStatus == "success" && element == "post"){
+                set_up_read_more();
                 var elem = document.getElementsByClassName(className);
                 elem[0].removeAttribute("class");
                 document.getElementById(element).setAttribute("class", className);
             }
-            if(textStatus == "error"){
-                alert("Erro ao carregar a página");
+            else{
+                if(textStatus == "success"){
+                    var elem = document.getElementsByClassName(className);
+                    elem[0].removeAttribute("class");
+                    document.getElementById(element).setAttribute("class", className);
+                }
+                else{
+
+                    if(textStatus == "error"){
+                        alert("Erro ao carregar a página");
+                    }
+
+                }
             }
 
         });
